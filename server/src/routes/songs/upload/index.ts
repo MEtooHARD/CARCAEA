c301b34fd83da3a4b9159bdbb93ed252995812d8
MEtooHARD <polar940301@gmail.com>
2026-01-16 10:00:44 +0800

    fix yt-dlp

--- server/src/routes/songs/upload/index.ts
+++ server/src/routes/songs/upload/index.ts
@@ -51,7 +51,7 @@ async function downloadYoutubeAudioWithProgress(
             '--audio-format', 'wav',
             '--postprocessor-args', 'ffmpeg:-ar 16000 -ac 1',
             '-o', path.join(AUD_TMP_DIR, `${file_name}.%(ext)s`),
-            '--extractor-args', 'youtube:player_client=ios',
+            '--extractor-args', 'youtube:player_client=mweb',
             '--socket-timeout', '30',
             '--retries', '5',
             '--fragment-retries', '5',
